5f18d0ece2e61ccbea07d16ac79f681c9acb37f9
govindbagdi <govindrajput747040@gmail.com>
2025-09-20 10:21:22 +0530

    trial commit

--- src/Hello.js
+++ src/Hello.js
@@ -3,8 +3,7 @@ function Hello()
     return(
         <div>
             <h1>Hello World</h1>
-            <p>My first react app</p>
-            <h1>my name is go </h1>
+            
         </div>
     )
 }export default Hello;
